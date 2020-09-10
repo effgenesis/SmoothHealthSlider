c5de4a6fa169d1bb053261d1ebad1dc76bd74738
effgenesis <56813503+effgenesis@users.noreply.github.com>
2020-09-10 13:40:54 +0300

    Add files via upload

---
+++ Assets/Scripts/SpellSlot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellSlot : MonoBehaviour
+{
+    [SerializeField] private Spells.Type _spell;
+    [SerializeField] public Health Target;
+
+    public void Cast()
+    {
+        Target.EffectedBySpell(_spell);
+    }
+}
---
+++ Assets/Scripts/Spells.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Spells
+{
+    public enum Type
+    {
+        Heal,
+        Harm
+    }
+
+    public static Dictionary<Type, float> _spellsPower = new Dictionary<Type, float>
+    {
+        {Type.Heal, 10f },
+        {Type.Harm, -10f }
+    };
+
+    public static float GetPower(Type type)
+    {
+        return _spellsPower[type];
+    }
+}
