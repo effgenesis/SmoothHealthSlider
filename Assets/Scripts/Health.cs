e57fd7439f560a76c7c9083ca458e521f818ea69
effgenesis <56813503+effgenesis@users.noreply.github.com>
2020-09-11 10:32:09 +0300

    Add files via upload

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,41 +7,41 @@ using DG.Tweening;
 [RequireComponent(typeof(Slider))]
 public class Health : MonoBehaviour
 {
-    public float _value { get; private set; } = 75;
-    [SerializeField] private float _maxValue;
-    [SerializeField] private Image _fill;
-    private Slider _slider;
+    public float _healthValue { get; private set; } = 75;
+    [SerializeField] private float _maxHealthValue;
+    [SerializeField] private Image _healthBarFill;
+    private Slider _healthBarSlider;
 
 
     public void Awake()
     {
-        _slider = GetComponent<Slider>();
-        _slider.maxValue = _maxValue;
-        _slider.value = _value;
+        _healthBarSlider = GetComponent<Slider>();
+        _healthBarSlider.maxValue = _maxHealthValue;
+        _healthBarSlider.value = _healthValue;
     }
 
     public void EffectedBySpell(Spells.Type type)
     {
-        _value += Spells.GetPower(type);
-        if (_value <= 0)
-            _value = 0;
-        if (_value > _maxValue)
-            _value = _maxValue;
-        _slider.DOValue(_value, 1);
+        _healthValue += Spells.GetPower(type);
+        if (_healthValue <= 0)
+            _healthValue = 0;
+        if (_healthValue > _maxHealthValue)
+            _healthValue = _maxHealthValue;
+        _healthBarSlider.DOValue(_healthValue, 1);
     }
 
     public void UpdateBarColor()
     {
         float normalHealthLimit = 0.5f;
         float lowHealthLimit = 0.2f;
-        if (_slider.value > _slider.maxValue * normalHealthLimit)
+        if (_healthBarSlider.value > _healthBarSlider.maxValue * normalHealthLimit)
         {
-            _fill.color = Color.Lerp(Color.yellow, Color.green, (_slider.value / _slider.maxValue - normalHealthLimit) / normalHealthLimit);
+            _healthBarFill.color = Color.Lerp(Color.yellow, Color.green, (_healthBarSlider.value / _healthBarSlider.maxValue - normalHealthLimit) / normalHealthLimit);
         }
-        else if (_slider.value > _slider.maxValue * lowHealthLimit)
+        else if (_healthBarSlider.value > _healthBarSlider.maxValue * lowHealthLimit)
         {
-            _fill.color = Color.Lerp(Color.red, Color.yellow, (_slider.value / _slider.maxValue - lowHealthLimit) / lowHealthLimit);
+            _healthBarFill.color = Color.Lerp(Color.red, Color.yellow, (_healthBarSlider.value / _healthBarSlider.maxValue - lowHealthLimit) / lowHealthLimit);
         }
-        else _fill.color = Color.red;
+        else _healthBarFill.color = Color.red;
     }
 }
